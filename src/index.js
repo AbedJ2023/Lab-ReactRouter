626000afdff9137b31c81956a60788641e750411
Abed <abedj2023@Gmail.com>
2023-02-01 10:18:37 -0600

    added Routes

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Red = (props) => {
+  return (
+    <div className="red">
+      <h1>RED</h1>
+    </div>
+  );
+};
+
+const Blue = (props) => {
+  return (
+    <div className="blue">
+      <h1>BLUE</h1>
+    </div>
+  );
+};
+
+const Main = (props) => {
+  return (
+    <BrowserRouter>
+      <div id="container">
+        <div id="navbar">
+          <Link to="/Blue">Go to Blue</Link>
+          <Link to="/Red">Go to Red</Link>
+        </div>
+
+        <div id="main-section">
+          <Route path="/blue">
+            <Blue />
+          </Route>
+          <Route path="/red">
+            <Red />
+          </Route>
+          <Route exact path="/">
+            <Red />
+          </Route>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+const app = document.getElementById("app");
+ReactDOM.render(<Main />, app);
